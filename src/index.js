4394deb9abdbfeef053c47d4e4e82014f57e06c5
Osmani <rtsxdfr@gmail.com>
2022-12-28 18:37:28 -0500

    Finished up the ui

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const start_btn = document.querySelector('#start');
+const pause_btn = document.querySelector('#pause');
+const reset_btn = document.querySelector('#reset');
+
+const time_display = document.querySelector('.time_display');
