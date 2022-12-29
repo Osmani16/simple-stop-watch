c8f9df5aa7d80f4b157ba9c630a6a87df3befe74
Osmani <rtsxdfr@gmail.com>
2022-12-29 13:00:27 -0500

    comments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+// ELEMENTS
 const start_btn = document.querySelector('#start');
 const pause_btn = document.querySelector('#pause');
 const reset_btn = document.querySelector('#reset');
 const time_display = document.querySelector('.time_display');
 let seconds_in_minute = 60;
 
+// GLOBALS
 let seconds_passed = 0;
 let minutes_passed = 0;
 let hours_passed = 0;
@@ -13,20 +15,29 @@ let is_paused;
 let initial_start;
 let stop_watch;
 
+// pause button is disabled at first. (you cant pause nothing)
 pause_btn.disabled = true;
 
+//function for single digits to be displayed with 0. ex: 5 -> 05
 const zero_pad = (num, places) => String(num).padStart(places, '0');
 
 start_btn.addEventListener('click', () => {
+    // disabling start when you start and enabling pause.
     start_btn.disabled = true;
     pause_btn.disabled = false;
     is_paused = false;
     const count = () => {
+        // initial_start makes sure that your not making a new timer every time.
         initial_start = true;
+        // if the timer is not paused.
         if (!is_paused) {
+            // adding 1000 miliseconds (or 1 second) every second.
             seconds += 1000;
+            // dividing by 1000 to convert it to seconds.
             seconds_passed = seconds / 1000;
 
+            // this checks if the next second will make it a new minute.
+            // if it does make it a new minute then it sets the seconds to a negative second to "restart" the count. with 0 first.
             if ((seconds_passed + 1) % seconds_in_minute == 0) {
                 seconds = -1000;
             }
