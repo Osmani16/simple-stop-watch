5060d8cc4385b8f4c78ece46e5d230ea973a2da5
Osmani <rtsxdfr@gmail.com>
2022-12-29 00:24:08 -0500

    Seconds/Minute reset. changed ui.

--- src/index.js
+++ src/index.js
@@ -1,42 +1,47 @@
 const start_btn = document.querySelector('#start');
 const pause_btn = document.querySelector('#pause');
 const reset_btn = document.querySelector('#reset');
-
 const time_display = document.querySelector('.time_display');
 
-let sec_num = 10;
-let min_num = sec_num ** 2;
+let seconds_in_minute = 60;
 
-let start_time = 0;
-let pause_time = 0;
 let seconds_passed = 0;
 let minutes_passed = 0;
 let hours_passed = 0;
 
 let time_elapsed;
 
+let seconds = 0;
+
 const zero_pad = (num, places) => String(num).padStart(places, '0');
 
 start_btn.addEventListener('click', () => {
-    const start_date = new Date();
-    start_time = start_date.getTime();
+    const count = () => {
+        seconds += 1000;
+        seconds_passed = seconds / 1000;
 
-    const count_down = () => {
-        let temp_date = new Date();
-        seconds_passed = Math.floor((temp_date.getTime() - start_time) / 1000);
-        if ((seconds_passed) % sec_num == 0) {
+        if ((seconds_passed + 1) % seconds_in_minute == 0) {
+            seconds = -1000;
+        }
+
+        if ((seconds_passed % seconds_in_minute) == 0) {
             minutes_passed++;
         }
-        if ((seconds_passed) % min_num == 0) {
+
+        if (minutes_passed % seconds_in_minute == 0 && minutes_passed != 0) {
             hours_passed++;
+            minutes_passed = 0;
         }
         time_elapsed = `${zero_pad(hours_passed, 2)}:${zero_pad(minutes_passed, 2)}:${zero_pad(seconds_passed, 2)}`;
         time_display.innerText = time_elapsed;
         reset_btn.addEventListener('click', () => {
             clearInterval(stop_watch);
+            seconds = 0;
+            seconds_passed = 0;
+            minutes_passed = 0;
+            hours_passed = 0;
             time_display.innerText = `00:00:00`;
         });
     };
-
-    const stop_watch = setInterval(count_down, 1000)
+    const stop_watch = setInterval(count, 1000)
 });
